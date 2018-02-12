bfccab4585e7a17384a116093fe80371f629bebe
Mirko <mirkoskroch@gmail.com>
2018-02-12 18:58:43 +0100

    FindComponentInParents added to ExtensionMethods
    For use with OnTrigger methods, since they can't detect compound colliders.

--- Assets/Scripts/Utility/ExtensionMethods.cs
+++ Assets/Scripts/Utility/ExtensionMethods.cs
@@ -103,6 +103,32 @@ public static class ExtensionMethods {
 
 
 
+    #region Collider
+    /// <summary>
+    /// Begins at the hit.transform and goes up in the hierarchy. 
+    /// Returns the fist component of type T found.
+    /// Useful for OnTrigger methods. Since they don't understand compound colliders, you can get the parent object by looking for a Rigidbody.
+    /// </summary>
+    public static T FindComponentInParents<T>(this Collider hit) where T : Component
+    {
+        Transform transform = hit.transform;
+        while (transform.GetComponent<T>() == null)
+        {
+            if (transform.parent == null)
+            {
+                Debug.LogError("Expected to find component of type "
+               + typeof(T) + " in parents of " + hit.name + ", but found none.");
+                break;
+            }
+
+            transform = transform.parent;
+        }
+        return transform.GetComponent<T>();
+    }
+    #endregion
+
+
+
     #region General Static Helper Methods
     /// <summary>
     /// Checks if a value is in range of a defined target value.
